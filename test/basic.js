950342582b6598bbe93c96a2409a7b0d2d47e155
Dominic Smith <code@smithamax.me>
2017-03-15 15:32:37 +1100

    No only

--- test/basic.js
+++ test/basic.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import httpRpcClient from '../';
 import mockService from './helpers/mock-service';
 
-test.only('call ping', async t => {
+test('call ping', async t => {
   const {end, address} = await mockService.create();
 
   const client = httpRpcClient.load(address, 'test-service');
